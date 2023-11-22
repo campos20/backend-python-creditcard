1d79cec70eaaad75654a4b7de1243ec06d01bb91
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2023-11-22 18:08:03 -0300

    Introduces listing endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import Depends, FastAPI, status
 from config.database import Base, SessionLocal, engine
 
-from dto.credit_card_dto import CreditCardDto
 from model.credit_card import CreditCard
 from sqlalchemy.orm import Session
+
+from schemas import CreditCardCreate, CreditCardDto
 
 Base.metadata.create_all(bind=engine)
 
@@ -18,15 +19,36 @@ def get_db():
         db.close()
 
 
-@app.post("/api/v1/credit-card", status_code=status.HTTP_201_CREATED)
-def create_credit_card(credit_card_dto: CreditCardDto, db: Session = Depends(get_db)):
-    db_credit_card = CreditCard()
-    db_credit_card.card_holder = credit_card_dto.card_holder
-    db_credit_card.card_number = credit_card_dto.card_number
-    db_credit_card.expiration_date = credit_card_dto.expiration_date
-    db_credit_card.cvv = credit_card_dto.cvv
+@app.post("/api/v1/credit-cards", status_code=status.HTTP_201_CREATED)
+def create_credit_card(
+    credit_card_dto: CreditCardCreate, db: Session = Depends(get_db)
+):
+    db_credit_card = CreditCard(**credit_card_dto.model_dump())
 
     db.add(db_credit_card)
     db.commit()
     db.refresh(db_credit_card)
-    return credit_card_dto
+
+    return CreditCardDto(**credit_card_dto.model_dump(), id=db_credit_card.id)
+
+
+@app.get("/api/v1/credit-cards")
+def list_credit_cards(
+    page: int = 0, page_size: int = 10, db: Session = Depends(get_db)
+):
+    offset = (page - 1) * page_size
+    total_count = db.query(CreditCard.id).count()
+    total_pages = (total_count + page_size - 1) // page_size
+
+    items = db.query(CreditCard).offset(offset).limit(page_size).all()
+
+    # Pydantically parse objects to avoid leaking details of the model
+    content = [CreditCardDto(**item.__dict__) for item in items]
+
+    return {
+        "content": content,
+        "page": page,
+        "page_size": page_size,
+        "total_pages": total_pages,
+        "total": total_count,
+    }
